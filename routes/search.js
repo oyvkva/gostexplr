33d9bd112ca027e88c1e1fa59b96eca4b3985cdb
xcps <xcps2p@yandex.com>
2018-02-04 12:12:09 +0500

    Detect by search string length what to search for

--- routes/search.js
+++ routes/search.js
@@ -26,38 +26,41 @@ router.post('/', async function(req, res, next) {
     }
   }
 
-  // looking for address
-  const address = await models.Address.findOne({
-    where: {
-      address: search,
-    },
-  });
-  if (address) {
-    res.redirect(`/address/${address.address}/`);
-    return;
+  if (search.length === 34) {
+    // looking for address
+    const address = await models.Address.findOne({
+      where: {
+        address: search,
+      },
+    });
+    if (address) {
+      res.redirect(`/address/${address.address}/`);
+      return;
+    }
+  } else if(search.length === 64) {
+    // looking for transaction
+    const transaction = await models.Transaction.findOne({
+      where: {
+        txid: search,
+      },
+    });
+    if (transaction) {
+      res.redirect(`/transaction/${transaction.txid}/`);
+      return;
+    }
+
+    // looking for block
+    const block = await models.Block.findOne({
+      where: {
+        hash: search,
+      },
+    });
+    if (block) {
+      res.redirect(`/block/${block.hash}/`);
+      return;
+    }
   }
 
-  // looking for transaction
-  const transaction = await models.Transaction.findOne({
-    where: {
-      txid: search,
-    },
-  });
-  if (transaction) {
-    res.redirect(`/transaction/${transaction.txid}/`);
-    return;
-  }
-
-  // looking for block
-  const block = await models.Block.findOne({
-    where: {
-      hash: search,
-    },
-  });
-  if (block) {
-    res.redirect(`/block/${block.hash}/`);
-    return;
-  }
   res.status(404).render('404');
 });
 
