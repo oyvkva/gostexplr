d9392547a00a4306aed1a56b7024486e1dfae096
Oyvkva <oyvind@kvanes.no>
2018-07-20 18:24:21 +0200

    UI updates

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
 
-  const blocks = await models.Block.findAll({
+  var blocks = await models.Block.findAll({
     attributes: ['height', 'hash', 'time', 'difficulty'],
     order: [['height', 'DESC']],
     limit: 100,
+  });
+  blocks.forEach(function (arrayItem) {
+    arrayItem.ago = arrayItem.time.toUTCString().substring(5);
   });
   res.render('index', {
     blocks,
